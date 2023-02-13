f839143ad6318d2ca67d7fdff00f812b9efe3936
Johnny Bhojwani <johnny_bhojwani@baylor.edu>
2023-02-13 12:38:57 -0600

    Initial Commit

---
+++ ProcessEmployees.py
@@ -0,0 +1,41 @@
+'''
+Due the outstanding performance of the Customer Service Represetatives (CSRs) in the marketing department, management
+has decided to reward them with a 10% increase in their salary. To evaluate the impact on the budget, you have been
+asked to run a report on the employee file and display the results of before and after the salary increase.
+
+You will create a dictionary that has the full employee name as the key and ONLY their new salary as the value.
+
+Iternate through the dictionary to print out their name and thier new salary (as in image)
+'''
+
+import csv
+
+#open the file
+
+
+
+
+#create an empty dictionary
+
+
+#use a loop to iterate through the csv file
+
+
+    #check if the employee fits the search criteria
+
+
+    
+
+print()
+print('=========================================')
+print()
+
+#iternate through the dictionary and print out the key and value as per printout
+
+
+
+          
+        
+
+        
+    
